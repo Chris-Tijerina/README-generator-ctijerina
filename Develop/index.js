544954c593905ce107cc02c2a310bf405c5897bc
Christopher <ctijerina@live.com>
2022-01-07 16:35:10 -0600

    installed inquirer and created first set of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input", 
+            name: "project",
+            message: "What is the name of your project?(Required)"
+        },
+        {
+            type: "input", 
+            name: "github", 
+            message: "Enter your GitHub username (Required)"  
+        }, 
+        {
+            type: "input", 
+            name: "email", 
+            message: "Enter your email address (Required)" 
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +30,8 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+questions()
+    .then(questions => {
+        console.log(questions)
+    });
