68ae871e00dfac6e2dd55630c04078296176d182
Christopher <ctijerina@live.com>
2022-01-08 21:55:06 -0600

    added the rest of the questions into the same array, but may reak them into different sections.

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,58 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-
+const generatePage = require("./utils/generateMarkdown")
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
             type: "input", 
-            name: "project",
-            message: "What is the name of your project?(Required)"
+            name: "title",
+            message: "What is the name of your project?"
         },
         {
             type: "input", 
             name: "github", 
-            message: "Enter your GitHub username (Required)"  
+            message: "Enter your GitHub username:"  
         }, 
         {
             type: "input", 
             name: "email", 
-            message: "Enter your email address (Required)" 
+            message: "Enter your email address:" 
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a description of your project:"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: " Provide a step-by-step description of how to get the development environment running:"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions for the use of your project:"
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Provide instructions for anyone who would like to contribute to your project:"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Leave examples or suggestions of tests that can be run on your project:"
+        }, 
+        {
+            type:"checkbox",
+            name: "licenses",
+            message: "What licenses would you like to include for your project? (check all that apply)",
+            choices: ["Apache 2.0", "Mozilla 2.0", "GitHub", "NPM", "Perl", "Eclipse", "GNU"]
         }
+
+
     ])
 }
 
